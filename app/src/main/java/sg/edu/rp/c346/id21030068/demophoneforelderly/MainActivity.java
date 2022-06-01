02685fc0ff208c1ad92e1a1b206d99b55c5ca5cf
TANG KEE KONG <21030068@myrp.edu.sg>
2022-06-01 16:15:32 +0800

    Lesson 6

---
+++ app/src/main/java/sg/edu/rp/c346/id21030068/demophoneforelderly/MainActivity.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.c346.id21030068.demophoneforelderly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button CallSon;
+    Button CallDaughter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CallSon = findViewById((R.id.buttonCallSon));
+        CallDaughter = findViewById(R.id.buttonCallDaughter);
+
+        CallSon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent. ACTION_DIAL, Uri.parse("tel:" + "92240336"));
+                startActivity(intent);
+            }
+        });
+
+        CallDaughter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent. ACTION_DIAL, Uri.parse("tel:" + "89675543"));
+                startActivity(intent);
+             }
+        });
+    }
+}
